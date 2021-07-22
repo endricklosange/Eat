7712cfc5a1281461be87315f83479d4f58268bae
endricklosange <endricklosange@gmail.com>
2021-07-22 02:46:59 +0200

    form register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,44 +5,43 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
-class RegistrationType extends AbstractType
+class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name', TextType::class, [
-                'label' => 'Prénom',
+                'label' => 'Pseudo',
                 'attr' => [
-                    'placeholder' => 'Jean'
+                    'placeholder' => 'randomdu45'
                 ]
             ])
-            
             ->add('email', EmailType::class, [
                 'attr' => [
-                    'placeholder' => 'jean.dupond@Email.fr'
+                    'placeholder' => 'random@Email.fr'
                 ]
             ])
-
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
-                'label' => 'Mot de passe',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Veuillez entrer un mot de passe',
+                        'message' => 'Please enter a password',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Ton mot de passe doit faire plus de {{ limit }} caractères',
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
